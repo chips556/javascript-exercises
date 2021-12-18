0219e01362222900bd31d41cc2644a575074be2b
chips556 <lohqihern@hotmail.com>
2021-12-18 17:51:21 +0800

    Finished exercises 7 & 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9;
+    celsius = parseFloat(celsius.toFixed(1), 10)
+    return celsius
 };
-
-const ctof = function() {
-
+const ctof = function (celsius) {
+    let fahrenheit = (celsius * 9 / 5) + 32;
+    fahrenheit = parseFloat(fahrenheit.toFixed(1), 10)
+    return fahrenheit
 };
-
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length > 0) {
+	return arr.reduce((a, b) => a + b)
+  }
+  return 0
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return Math.pow(num, pow)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 0) {
+    return 1
+  }
+
+  return factorial(num - 1) * num
 };
 
 // Do not edit below this line
